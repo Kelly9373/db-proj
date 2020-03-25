703b33b506f00d300121216452a23e3896adc39e
Jiang Wulin <wulinjiang666@gmail.com>
2020-03-25 19:19:22 +0800

    groupby operator

--- src/qp/operators/GroupBy.java
+++ src/qp/operators/GroupBy.java
@@ -1,4 +1,13 @@
 package qp.operators;
 
-public class GroupBy {
+import java.util.*;
+
+/**
+ * GroupBy Operator inherit from Distinct Operator
+ */
+public class GroupBy extends Distinct {
+
+    public GroupBy(Operator base, ArrayList attrs) {
+        super(base, attrs);
+    }
 }
