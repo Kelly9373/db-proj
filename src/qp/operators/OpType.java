cc55ef055c892f2e695241d5489c636182e49510
Jiang Wulin <wulinjiang666@gmail.com>
2020-03-25 17:05:46 +0800

    External sort and distinct

--- src/qp/operators/OpType.java
+++ src/qp/operators/OpType.java
@@ -12,6 +12,7 @@ public class OpType {
     public static final int JOIN = 3;
     public static final int DISTINCT = 4;
     public static final int GROUPBY = 5;
+    public static final int SORT = 6;
 
 
 
